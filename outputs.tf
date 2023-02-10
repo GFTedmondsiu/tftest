0633958143d7d202fbbfb387eb6e899eb82f8895
GFTedmondsiu <124569710+GFTedmondsiu@users.noreply.github.com>
2023-02-10 09:07:04 +0000

    terraform fmt

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "this" {
-    value = data.local_file.this.content
+  value = data.local_file.this.content
 }
--- providers.tf
+++ providers.tf
@@ -2,10 +2,10 @@ terraform {
   required_providers {
     aws = {
       version = ">= 2.7.0"
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
     }
     null = {
-      source = "hashicorp/null"
+      source  = "hashicorp/null"
       version = "3.2.1"
     }
   }
